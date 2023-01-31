b213c20b656e801a98713c80bf601156fbdcb9e5
oriverol <oriverol@cjc.edu.bz>
2023-01-31 13:48:50 -0600

    fix

--- mainOutputForm.vb
+++ mainOutputForm.vb
@@ -2,6 +2,6 @@
     Private Sub mainInputForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
         outputFormPublic.Text = "The last name of the person that just logged in is: " & loginForm.stLname & ", his first name is: " & loginForm.stFname
-        'yo'
+
     End Sub
 End Class

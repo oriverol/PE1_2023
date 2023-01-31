0a9c5d9a389606d93eb7e5eb3c42ea831f6feaed
oriverol <oriverol@cjc.edu.bz>
2023-01-31 13:33:53 -0600

    Add project files.

---
+++ loginForm.vb
@@ -0,0 +1,32 @@
+﻿Public Class loginForm
+
+    Public stFname As String
+    Public stLname As String
+    Public stFullName As String
+
+    Private Sub loginForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+
+        stFname = txb_FName.Text
+        stLname = txb_LName.Text
+        stFullName = stFname & " " & stLname
+
+        publicVariableOutput.Text = stFullName
+
+        formattedPublicVariableOutput.Text = stLname & ", " & stFname
+
+        textControlOutput.Text = txb_FName.Text & " " & txb_LName.Text
+
+        mainOutputForm.Show()
+
+    End Sub
+
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+        Application.Exit()
+
+    End Sub
+
+
+End Class
---
+++ mainOutputForm.Designer.vb
@@ -0,0 +1,51 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class mainOutputForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.outputFormPublic = New System.Windows.Forms.RichTextBox()
+        Me.SuspendLayout()
+        '
+        'outputFormPublic
+        '
+        Me.outputFormPublic.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.outputFormPublic.ForeColor = System.Drawing.Color.Red
+        Me.outputFormPublic.Location = New System.Drawing.Point(59, 51)
+        Me.outputFormPublic.Name = "outputFormPublic"
+        Me.outputFormPublic.Size = New System.Drawing.Size(339, 164)
+        Me.outputFormPublic.TabIndex = 1
+        Me.outputFormPublic.Text = ""
+        '
+        'mainOutputForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(453, 281)
+        Me.Controls.Add(Me.outputFormPublic)
+        Me.Name = "mainOutputForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "mainInputForm"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents outputFormPublic As RichTextBox
+End Class
